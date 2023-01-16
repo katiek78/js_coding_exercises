c4c84e5a6abea7714bd7c155399a70b566d49e35
katiek78 <kermodekatie@gmail.com>
2023-01-16 22:29:26 +0000

    Removed logging from addVAT in exercise001

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -17,7 +17,6 @@ export function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
   if (vatRate === undefined) throw new Error("vatRate is required");
   // Add your code here!
-  console.log( originalPrice + vatRate/100*originalPrice);
   return Number((originalPrice + vatRate/100*originalPrice).toFixed(2));
 }
 
