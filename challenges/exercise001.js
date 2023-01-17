67d1b171c7874fcad3dca4173a9747b6ec6b2661
katiek78 <kermodekatie@gmail.com>
2023-01-17 09:43:28 +0000

    Completed simpleFizzBuzz from exercise001

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -64,5 +64,13 @@ export function getMeanScore(scores) {
 export function simpleFizzBuzz(n) {
   if (n === undefined) throw new Error("n is required");
   // Add your code here!
-
+  if (isNaN(n)) throw new Error("n must be numeric");
+  if (n === 0) return 0;
+  if (n % 3 === 0 && n % 5 === 0) {
+    return "fizzbuzz";
+  } else if (n % 3 === 0) {
+    return "fizz";
+  } else if (n % 5 === 0) {
+    return "buzz";
+  } else return n;
 }
