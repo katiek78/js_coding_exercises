eddedf4e69de5bed16a078c829d21cb5f6ffc997
katiek78 <kermodekatie@gmail.com>
2023-01-17 17:19:22 +0000

    Completed getTotalSubjects from exercise003

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -10,12 +10,14 @@ export function camelCaseWords(words) {
   if (words === undefined) throw new Error("words is required");
   // Your code here!
   if (!Array.isArray(words)) throw new Error("words must be an array");
-  return words.map((word,idx) => idx === 0 ? word : word[0].toUpperCase() + word.slice(1)).join("");
+  return words.map((word, idx) => idx === 0 ? word : word[0].toUpperCase() + word.slice(1)).join("");
 }
 
 export function getTotalSubjects(people) {
   if (people === undefined) throw new Error("people is required");
   // Your code here!
+  if (!Array.isArray(people)) throw new Error("people must be an array");
+  return people.reduce((acc, val) => val.subjects ? acc + val.subjects.length : acc, 0);
 }
 
 export function checkIngredients(menu, ingredient) {
