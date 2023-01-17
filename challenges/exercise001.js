86a84ccc05aa92e83ed67589ca41458a6a04a211
katiek78 <kermodekatie@gmail.com>
2023-01-17 09:04:06 +0000

    Completed getSalePrice from exercise001

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -24,6 +24,7 @@ export function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (reduction === undefined) throw new Error("reduction is required");
   // Add your code here!
+  return Number((originalPrice - reduction/100*originalPrice).toFixed(2));
 }
 
 export function getMiddleCharacter(str) {
