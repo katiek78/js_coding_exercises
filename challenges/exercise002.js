82cfb43eb540451393edbba7f2910a820f1295c1
katiek78 <kermodekatie@gmail.com>
2023-01-21 15:12:45 +0000

    Improved equality checking in exercise002

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,38 +1,34 @@
 export function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
-  if (sandwich == null) throw new Error("sandwich must not be null");
-  if (typeof sandwich != 'object') throw new Error("sandwich must be an object");
+  if (typeof sandwich !== 'object' || sandwich === null) throw new Error("sandwich must be an object");
+
   return sandwich.fillings;
 }
 
 export function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
-  if (person == null) throw new Error("person must not be null");
-  if (typeof person != 'object') throw new Error("person must be an object");
-  return (person.city === 'Manchester');
+  if (typeof person !== 'object' || person === null) throw new Error("person must be an object");
+
+  return person.city === 'Manchester';
 }
 
 export function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
-  if (isNaN(people)) throw new Error("people must be numeric");
-  return (Math.ceil(people / 40));
+  if (isNaN(people) || people === null) throw new Error("people must be numeric");
+
+  return Math.ceil(people / 40);
 }
 
 export function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
   if (!Array.isArray(arr)) throw new Error("arr must be an array");
+
   return arr.reduce((acc, val) => val === "sheep" ? ++acc : acc, 0);
 }
 
 export function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
-  if (person == null) throw new Error("person must not be null");
-  if (typeof person != 'object') throw new Error("person must be an object");
-  if (!person.address || !person.address.postCode) return false;
-  return (person.address.postCode[0] === "M" && (!/[A-Za-z]/.test(person.address.postCode[1])));
+  if (typeof person !== 'object' || person === null) throw new Error("person must be an object");
+
+  return (person.address?.postCode[0] === "M" && (!/[A-Za-z]/.test(person.address.postCode[1])));
 }
